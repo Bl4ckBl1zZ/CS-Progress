ba427f6d9a264401f1c7a1f83f8613192070fd2f
Bl4ckBl1zZ <59123836+Bl4ckBl1zZ@users.noreply.github.com>
2021-05-03 14:21:06 +0200

    a

--- 20200502_Calculator/20200502_Calculator/Program.cs
+++ 20200502_Calculator/20200502_Calculator/Program.cs
@@ -6,7 +6,46 @@ namespace _20200502_Calculator
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int number1;
+            int number2;
+            int operation;
+            int answer;
+
+            Console.WriteLine("Welcome to my first Calculator!");
+            Console.WriteLine("Please type in your first number!");
+            number1 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type in your second number!");
+            number2 = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Please type in one of the following operations: +, -, :, *.");
+            operation = Convert.ToInt32(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "+":
+                    answer = number1 + number2;
+                    break;
+
+                case "-":
+                    answer = number1 - number2;
+                    break;
+
+                case "*":
+                    answer = number1 * number2;
+                    break;
+
+                case ":":
+                    answer = number1 / number2;
+                    break;
+
+                default:
+                    answer = 0;
+                    break;
+            }
+
+            Console.WriteLine(number1() + " " + operation + " " + number2 + " = " + answer);
+
         }
     }
 }
