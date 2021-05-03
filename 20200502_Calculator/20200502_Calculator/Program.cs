49630dacfa394a0b6abe002b721e6deea6ab9e51
Bl4ckBl1zZ <59123836+Bl4ckBl1zZ@users.noreply.github.com>
2021-05-03 09:20:11 +0200

    Project Started

---
+++ 20200502_Calculator/20200502_Calculator/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _20200502_Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
