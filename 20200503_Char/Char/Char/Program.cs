714d8234146a0605fde673ddea3c44b89d502956
Bl4ckBl1zZ <davfab16@icloud.com>
2021-05-03 20:44:10 +0200

    Char

---
+++ 20200503_Char/Char/Char/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Char
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char singleÖetter = 'A';
+            Console.WriteLine(singleÖetter);
+
+            Console.ReadKey();
+        }
+    }
+}
