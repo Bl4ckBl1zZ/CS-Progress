16e03c52530a1253b1c2caa7fdbae77a756c6259
Bl4ckBl1zZ <davfab16@icloud.com>
2021-05-03 19:07:38 +0200

    Xenos Fixed alot off stuff :/

--- 20200502_Calculator/20200502_Calculator/Program.cs
+++ 20200502_Calculator/20200502_Calculator/Program.cs
@@ -1,53 +1,96 @@
 ﻿using System;
+using System.Threading;
 
 namespace _20200502_Calculator
 {
     class Program
     {
-        static void Main(string[] args)
+        public static int number1;
+        public static int number2;
+        public static string operation;
+        public static int answer;
+
+        static void Main()
         {
-            int number1;
-            int number2;
-            string operation;
-            float answer;
 
-            Console.WriteLine("Welcome to my first Calculator!");
-            Console.WriteLine("Please type in your first number!");
-            number1 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please type in your second number!");
-            number2 = Convert.ToInt32(Console.ReadLine());
-
-            Console.WriteLine("Please type in one of the following operations: +, -, :, *.");
-            operation = Convert.ToString(Console.ReadLine());
-
-            switch (operation)
+            try
             {
-                case "+":
-                    answer = number1 + number2;
-                    break;
 
-                case "-":
-                    answer = number1 - number2;
-                    break;
 
-                case "*":
-                    answer = number1 * number2;
-                    break;
+                Console.Clear();
+                Console.WriteLine("Welcome to my first Calculator!");
+                Console.WriteLine("Please type in your first number!");
 
-                case ":":
-                    answer = number1 / number2;
-                    break;
+                number1 = Int32.Parse(Console.ReadLine());
 
-                default:
-                    answer = 0;
-                    break;
+                Console.WriteLine("Please type in your second number!");
+                number2 = Int32.Parse(Console.ReadLine());
+
+                Console.WriteLine("Please type in one of the following operations: +, -, :, *.");
+                operation = Console.ReadLine();
+
+                if (number1 > Int32.MaxValue || number2 > Int32.MaxValue) 
+                {
+                    Console.WriteLine("Numbers to high to calculate.");
+                }
+
+                switch (operation)
+                {
+                    case "+":
+                        answer = number1 + number2;
+                        break;
+
+                    case "-":
+                        answer = number1 - number2;
+                        break;
+
+                    case "*":
+                        answer = number1 * number2;
+                        break;
+
+                    case ":":
+                        answer = number1 / number2;
+                        break;
+
+                    default:
+                        Console.WriteLine("Not a valid opertator");
+                        Thread.Sleep(2500);
+                        Main();
+                        break;
+                }
+
+                Console.WriteLine(number1.ToString() + " " + operation.ToString() + " " + number2.ToString() + " = " + answer.ToString());
+
+                Console.WriteLine("Do You have any more calculations to do?");
+                Console.WriteLine("yes or no?");
+                string repeat = Console.ReadLine();
+                switch (repeat)
+                {
+                    case "yes":
+                        Main();
+                        break;
+                    case "no":
+                        Environment.Exit(-1);
+                        break;
+                    default:
+                        Console.WriteLine("You can only use Yes or No");
+                        break;
+                }
+
+                Console.ReadKey();
             }
-
-            Console.WriteLine(number1.ToString() + " " + operation.ToString() + " " + number2.ToString() + " = " + answer.ToString());
-
-            Console.ReadKey();
-
+            catch (FormatException)
+            {
+                Console.WriteLine("Invalid number provided. ");
+                Thread.Sleep(2500);
+                Main();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"an unknown error orrcued: \n{ex.Message}");
+                Thread.Sleep(2500);
+                Main();
+            }
         }
     }
 }
