a64dc693197667f7879f65daefc417e730fc609d
Bl4ckBl1zZ <59123836+Bl4ckBl1zZ@users.noreply.github.com>
2021-05-06 15:58:09 +0200

    a

--- 20210506_Something/20210506_Something/Program.cs
+++ 20210506_Something/20210506_Something/Program.cs
@@ -18,13 +18,12 @@ namespace _20210506_Something
             switch (operator1)
             {
                 case "*":
-                    operator1 = "*" ;
+                    operator1 = "*";
                     break;
 
                 case "-":
                     operator1 = "-";
                     break;
-                    Console.Clear();
 
                 case "+":
                     operator1 = "+";
@@ -39,56 +38,25 @@ namespace _20210506_Something
                     Thread.Sleep(2500);
                     Main();
                     break;
+
+            }
+
+            Console.Clear();
 
             Console.WriteLine("Please type in your second number:");
             int num2 = Convert.ToInt32(Console.ReadLine());
 
             Console.WriteLine("What operator do you want use?");
-            string operator1 = Console.ReadLine();
-            switch (operator1)
-            {
-                case "*":
-                    operator1 = "*" ;
-                    break;
-
-                case "-":
-                    operator1 = "-";
-                    break;
-                    Console.Clear();
-
-                case "+":
-                    operator1 = "+";
-                    break;
-
-                case ":":
-                    operator1 = "/";
-                    break;
-
-                default:
-                    Console.WriteLine("Not a valid opertator");
-                    Thread.Sleep(2500);
-                    Main();
-                    break;
-
-            Console.Clear();
-
-
-
-            Console.WriteLine("Please type in your third number:");
-            int num3 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("What operator do you want use?");
             string operator2 = Console.ReadLine();
-
             switch (operator2)
             {
                 case "*":
-                    operator2 = "*" ;
+                    operator2 = "*";
                     break;
 
                 case "-":
                     operator2 = "-";
                     break;
-                    Console.Clear();
 
                 case "+":
                     operator2 = "+";
@@ -103,12 +71,56 @@ namespace _20210506_Something
                     Thread.Sleep(2500);
                     Main();
                     break;
-                            Console.Clear();
+
+            }
+
+            Console.Clear();
 
 
-            Console.WriteLine("Your calculation you wanted it is:");
+            Console.WriteLine("Please type in your third number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+
+            Console.Clear();
+
+
+
+            Console.WriteLine("Your calculation you wanted is:");
             int result = num1 * num2 * num3;
-            Console.WriteLine("{0} * {1} * {2} = {3}", num1, num2, num3, result);
+
+            if (result > Int32.MaxValue)
+            {
+                Console.WriteLine("Numbers to high to calculate.");
+            }
+
+            Console.WriteLine("{0} {1} {2} {3} {4} = {5}", num1, operator1, num2, operator2, num3, result);
+
+            Thread.Sleep(2500);
+
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Do you have any more calculations to do?");
+            Console.WriteLine("Yes or no?");
+
+            string repeat = Console.ReadLine();
+
+            switch (repeat)
+            {
+                case "yes":
+                    Main();
+                    break;
+                case "no":
+                    Console.WriteLine("thanks");
+                    Thread.Sleep(2500);
+                    Environment.Exit(-1);
+                    break;
+                default:
+                    Console.WriteLine("You can only use Yes or No");
+                    break;
+            }
+
+
 
 
             Console.ReadKey();
