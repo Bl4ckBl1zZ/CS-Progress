666f8fdece8f419ba2169619aa4b809298949b62
Bl4ckBl1zZ <59123836+Bl4ckBl1zZ@users.noreply.github.com>
2021-05-06 11:24:18 +0200

    a

---
+++ 20210506_Something/20210506_Something/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace _20210506_Something
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("calculator but with 3 numbers");
+            Console.WriteLine("");
+            Console.WriteLine("");
+            Console.WriteLine("Please type in your first number:");
+            int num1 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Please type in your second number:");
+            int num2 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Please type in your third number:");
+            int num3 = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+
+            Console.WriteLine("Your calculation you wanted it is:");
+            int result = num1 * num2 * num3;
+            Console.WriteLine("{0} * {1} * {2} = {3}", num1, num2, num3, result);
+
+
+            Console.ReadKey();
+        }
+    }
+}
